ba298606619fa37590fd5b9340ac9de711db031c
Luke Sheppard <sheppiegit@gmail.com>
2023-06-11 13:49:28 -0400

    Modified deploy to deploy all 5 game contracts.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,15 @@
-// replace the name of the contract with which one you want to deploy!
-const contractName = "Game1";
-
 async function main() {
-  const Game = await hre.ethers.getContractFactory(contractName);
-  // if you need to add constructor arguments for the particular game, add them here:
-  const game = await Game.deploy();
-  console.log(`${contractName} deployed to address: ${game.address}`);
+  for (let i = 1; i <= 5; i++) {
+    let contractName = `Game${i}`;
+    const Game = await hre.ethers.getContractFactory(contractName);
+    const game = await Game.deploy();
+    console.log(`${contractName} deployed to address: ${game.address}`);
+  }
 }
 
 main()
- .then(() => process.exit(0))
- .catch(error => {
-   console.error(error);
-   process.exit(1);
- });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
