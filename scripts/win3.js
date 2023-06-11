a70a90b4ab76898d5782a222606d930c05a76a14
Luke Sheppard <sheppiegit@gmail.com>
2023-06-11 14:00:38 -0400

    game 3

---
+++ scripts/win3.js
@@ -0,0 +1,18 @@
+const gameAddr = '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9';
+const contractName = 'Game3';
+
+async function main() {
+  const game = await hre.ethers.getContractAt(contractName, gameAddr);
+
+  const tx = await game.win(45);
+
+  const receipt = await tx.wait();
+  console.log(receipt);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
